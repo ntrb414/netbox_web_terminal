b48f375fd0e0cb8b82873c0f12a9f1629eee1634
seth <seth@ecoflow.com>
2026-01-13 15:25:19 +0800

    fix: change as_view() to as_asgi() in routing.py for Channels compatibility

--- netbox_web_terminal/routing.py
+++ netbox_web_terminal/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/terminal/<int:pk>/', consumers.TerminalConsumer.as_view()),
+    path('ws/terminal/<int:pk>/', consumers.TerminalConsumer.as_asgi()),
 ]
