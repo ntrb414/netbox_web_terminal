adde05149b0a7892b22bd16e7f03cc6b2b3f6376
seth <seth@ecoflow.com>
2026-01-13 18:14:54 +0800

    Update repository URL in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='netbox-web-terminal',
     version='0.1',
     description='A Web SSH Terminal plugin for NetBox',
-    url='https://github.com/your-repo/netbox-web-terminal',
+    url='https://github.com/ntrb414/netbox_web_terminal.git',
     author='Trae Assistant',
     license='Apache 2.0',
     install_requires=[
